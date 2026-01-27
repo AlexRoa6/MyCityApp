c9026ffca0038f6f83b6bec991b6925849e91e6b
AlexRoa6 <alexroasebastian9@gmail.com>
2026-01-27 10:11:20 +0100

    Primera subida

---
+++ app/src/main/java/com/example/p4_madrid_marcos_jorge_alejandro/ui/views/AppDestination.kt
@@ -0,0 +1,16 @@
+package com.example.p4_madrid_marcos_jorge_alejandro.ui.views
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.ui.graphics.vector.ImageVector
+
+enum class AppDestination (
+    val label: String,
+    val icon: ImageVector,
+) {
+    HOME("Home", Icons.Default.Home),
+    FAVORITES("Favorites", Icons.Default.Favorite),
+    PROFILE("Profile", Icons.Default.AccountBox),
+}
---
+++ app/src/main/java/com/example/p4_madrid_marcos_jorge_alejandro/ui/views/ShopingCenterView.kt
@@ -0,0 +1,47 @@
+package com.example.p4_madrid_marcos_jorge_alejandro.ui.views
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+
+
+
+
+@Composable
+fun ShopingCenterView(){
+    var currentDestination by rememberSaveable { mutableStateOf(AppDestination.HOME) }
+
+    NavigationSuiteScaffold(
+        navigationSuiteItems = {
+            AppDestination.entries.forEach {
+                item(
+                    icon = {
+                        Icon(
+                            it.icon,
+                            contentDescription = it.label
+                        )
+                    },
+                    label = { Text(it.label) },
+                    selected = it == currentDestination,
+                    onClick = { currentDestination = it }
+                )
+            }
+        }
+    ) {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+        }
+    }
+}
