89494bd860dcdf4eeb82bd76c2dd29e84701dc68
AlexRoa6 <alexroasebastian9@gmail.com>
2026-01-27 11:09:08 +0100

    shopinCenterUiState definido

--- app/src/main/java/com/example/p4_madrid_marcos_jorge_alejandro/viewModels/ShopingCenterViewModel.kt
+++ app/src/main/java/com/example/p4_madrid_marcos_jorge_alejandro/viewModels/ShopingCenterViewModel.kt
@@ -1,2 +1,19 @@
 package com.example.p4_madrid_marcos_jorge_alejandro.viewModels
 
+import com.example.p4_madrid_marcos_jorge_alejandro.model.ShopingCenter
+import com.example.p4_madrid_marcos_jorge_alejandro.ui.views.ShopingCenterView
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+data class ShopingCenterUiState(
+    val shopingCenters: List<ShopingCenter> = emptyList()
+)
+
+class ShopingCenterViewModel{
+
+    private val _uiSate = MutableStateFlow(ShopingCenterUiState())
+    val uiState: StateFlow<ShopingCenterUiState> =_uiSate.asStateFlow()
+
+
+}
