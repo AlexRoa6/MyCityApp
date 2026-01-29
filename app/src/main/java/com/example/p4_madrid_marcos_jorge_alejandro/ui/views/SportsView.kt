5b8bd4791e7963667f34383b7a3f1596b2c845f7
Marco <Desertmarkos@gmail.com>
2026-01-29 13:08:50 +0100

    Arreglo nombre fotos

--- app/src/main/java/com/example/p4_madrid_marcos_jorge_alejandro/ui/views/SportsView.kt
+++ app/src/main/java/com/example/p4_madrid_marcos_jorge_alejandro/ui/views/SportsView.kt
@@ -2,10 +2,5 @@ package com.example.p4_madrid_marcos_jorge_alejandro.ui.views
 
 import com.example.p4_madrid_marcos_jorge_alejandro.model.Sport
 
-data class SportUiState(
 
-    val listaSport: List<Sport> = emptyList()
-
-
-)
 
