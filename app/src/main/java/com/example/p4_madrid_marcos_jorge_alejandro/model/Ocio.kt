375fa75c019f75580a019097ca3ec96623c1c249
Jorge <j.martinez.nsp@gmail.com>
2026-01-29 14:19:19 +0100

    Parte de Ocio

--- app/src/main/java/com/example/p4_madrid_marcos_jorge_alejandro/model/Ocio.kt
+++ app/src/main/java/com/example/p4_madrid_marcos_jorge_alejandro/model/Ocio.kt
@@ -1,4 +1,8 @@
 package com.example.p4_madrid_marcos_jorge_alejandro.model
 
-class Ocio {
+
+class Ocio (nameRes: Int, imgRes: Int, descriptionRes: Int,  isExpanded: Boolean = false):
+    Site (nameRes, imgRes, descriptionRes, isExpanded) {
+
+
 }
