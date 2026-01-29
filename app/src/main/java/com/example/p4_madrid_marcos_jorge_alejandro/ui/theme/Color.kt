93854748befe0e32605974a1ad304d12d8db3765
AlexRoa6 <alexroasebastian9@gmail.com>
2026-01-29 10:56:25 +0100

    Colores y temas definidos

--- app/src/main/java/com/example/p4_madrid_marcos_jorge_alejandro/ui/theme/Color.kt
+++ app/src/main/java/com/example/p4_madrid_marcos_jorge_alejandro/ui/theme/Color.kt
@@ -2,10 +2,13 @@ package com.example.p4_madrid_marcos_jorge_alejandro.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val BackgroundLight = Color(0xfff8f9fa)
+val BackgroundDark = Color(0xFF101022)
+val PrimaryLight = Color(0xFF00b0ff)
+val PrimaryDark = Color(0xFF0d0df2)
+val CardBackgroundLight = Color(0xFFffffff)
+val CardBackgroundDark = Color(0xFF2E2E45)
+val SecundarioLight = Color(0xFF9ca3af)
+val SecundarioDark = Color(0xFF94a3b8)
+val TextColorLight = Color(0xFF1f2937)
+val TextColorDark = Color(0xFFffffff)
