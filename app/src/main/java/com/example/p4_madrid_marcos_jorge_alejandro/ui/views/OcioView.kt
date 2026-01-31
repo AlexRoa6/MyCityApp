2672c72178595a05e324a5faa83e80182f335844
AlexRoa6 <alexroasebastian9@gmail.com>
2026-01-31 12:04:34 +0100

    Implementados los componentes compartidos en las views

--- app/src/main/java/com/example/p4_madrid_marcos_jorge_alejandro/ui/views/OcioView.kt
+++ app/src/main/java/com/example/p4_madrid_marcos_jorge_alejandro/ui/views/OcioView.kt
@@ -1,7 +1,26 @@
 package com.example.p4_madrid_marcos_jorge_alejandro.ui.views
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.p4_madrid_marcos_jorge_alejandro.R
 import com.example.p4_madrid_marcos_jorge_alejandro.model.Ocio
+import com.example.p4_madrid_marcos_jorge_alejandro.ui.components.GenericScreenContent
+import com.example.p4_madrid_marcos_jorge_alejandro.viewModels.deportesViewModel
+import com.example.p4_madrid_marcos_jorge_alejandro.viewModels.ocioViewModel
 
-data class OcioUiState (
-    val listaOcio : List<Ocio> = emptyList()
-)
+@Composable
+fun OcioView(viewModel: ocioViewModel = viewModel()){
+    val uiState by viewModel.uiState.collectAsState()
+
+    GenericScreenContent(
+        items = uiState.ocioList,
+        selectedItem = uiState.selectedOcio,
+        onClickCard = { viewModel.onClickCard(it) },
+        onDismissModal = { viewModel.onDismissModal() },
+        titleRes = R.string.ocio_title,
+        descriptionRes = R.string.descripcion_ocio,
+        topBarTitleRes = R.string.ocio_title_topbar
+    )
+}
--- app/src/main/java/com/example/p4_madrid_marcos_jorge_alejandro/ui/views/SportsView.kt
+++ app/src/main/java/com/example/p4_madrid_marcos_jorge_alejandro/ui/views/SportsView.kt
@@ -1,129 +1,25 @@
 package com.example.p4_madrid_marcos_jorge_alejandro.ui.views
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.itemsIndexed
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.p4_madrid_marcos_jorge_alejandro.R
-import com.example.p4_madrid_marcos_jorge_alejandro.model.Categoria
-import com.example.p4_madrid_marcos_jorge_alejandro.model.Sport
+import com.example.p4_madrid_marcos_jorge_alejandro.ui.components.GenericScreenContent
 import com.example.p4_madrid_marcos_jorge_alejandro.viewModels.deportesViewModel
 
 @Composable
-fun SportsView(
-    viewModel: deportesViewModel
-) {
-    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+fun SportsView(viewModel: deportesViewModel = viewModel()){
+    val uiState by viewModel.uiState.collectAsState()
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background)
-            .padding(16.dp)
-    ) {
-
-        Text(
-            text = "Recommended for you",
-            style = MaterialTheme.typography.titleLarge,
-            fontWeight = FontWeight.Bold,
-            color = MaterialTheme.colorScheme.onBackground
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(2),
-            verticalArrangement = Arrangement.spacedBy(16.dp),
-            horizontalArrangement = Arrangement.spacedBy(16.dp)
-        ) {
-
-        }
-    }
+    GenericScreenContent(
+        items = uiState.deportesList,
+        selectedItem = uiState.selectedSport,
+        onClickCard = { viewModel.onClickCard(it) },
+        onDismissModal = { viewModel.onDismissModal() },
+        titleRes = R.string.deportes_title,
+        descriptionRes = R.string.descripcion_sport,
+        topBarTitleRes = R.string.deportes_title_topbar
+    )
 }
-
-@Composable
-fun SportCard(
-    sport: Sport,
-    onClick: () -> Unit
-) {
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .clickable { onClick() },
-        shape = RoundedCornerShape(22.dp),
-        elevation = CardDefaults.cardElevation(8.dp)
-    ) {
-        Box {
-
-            Image(
-                painter = painterResource(id = sport.imgRes),
-                contentDescription = null,
-                contentScale = ContentScale.Crop,
-                modifier = Modifier.fillMaxSize()
-            )
-
-            // Gradiente oscuro inferior (como la imagen)
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(
-                        Brush.verticalGradient(
-                            colors = listOf(
-                                Color.Transparent,
-                                Color.Black.copy(alpha = 0.75f)
-                            )
-                        )
-                    )
-            )
-
-            Column(
-                modifier = Modifier
-                    .align(Alignment.BottomStart)
-                    .padding(14.dp)
-            ) {
-
-                Text(
-                    text = stringResource(id = sport.nameRes),
-                    style = MaterialTheme.typography.titleMedium,
-                    fontWeight = FontWeight.Bold,
-                    color = Color.White
-                )
-
-
-            }
-        }
-    }
-}
-
-
-@Preview
-@Composable
-fun SportsView () {
-    SportCard(Sport(
-        categoria = Categoria.FUTBOL,
-        nameRes = R.string.Titulo_realmadrid,
-        descriptionRes = R.string.descripcion_realmadrid,
-        imgRes = R.drawable.brenabeu
-    ), onClick = {})
-}
-
-
-
